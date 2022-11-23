0bed83728dab9372e3a9b177c1b60f14138aae09
eptrubnikova <eptrubnikova@gmail.com>
2022-11-23 08:28:35 +0600

    lesson 2

--- skypro_course2/Main.java
+++ skypro_course2/Main.java
@@ -5,12 +5,17 @@ public class Main {
         Person anya = new Person("Аня", 1993, "Москва", "методист образовательных программ");
         Person katya = new Person("Катя", 0, "Калининград", "продакт-менеджер");
         Person artem = new Person("Артем", 1995, "Москва", "директор по развитию бизнеса");
-        Person mister = new Person(null, 1955, "", "");
+        Person mister = new Person(null, ' ', "", "");
+        Person vladimir = new Person("Владимир", 2001, "Казань", "не работаю");
 
+        mister.setTown();
+        mister.setYearOfBirth(' ');
         System.out.println(maksim);
         System.out.println(anya);
         System.out.println(katya);
         System.out.println(artem);
+        System.out.println(mister);
+        System.out.println(vladimir);
 
     }
 }
